2661966d5d639a697c268cb06a3896f31f292e5f
uma shankar anantharapu <umashankar.anantharapu97@gmail.com>
2025-08-28 18:56:41 +0530

    refactor(api): use environment variable for api base url

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private readonly apiBaseUrl = 'http://localhost:5000/api';
+  private readonly apiBaseUrl = environment.apiUrl;
 
   constructor(private readonly http: HttpClient) { }
 
